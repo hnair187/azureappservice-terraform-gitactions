400d2cb05dfc0451b1ec1a0b6d85d074de04ebd2
Hiral <77860339+hnair187@users.noreply.github.com>
2021-11-03 18:28:53 +0530

    azure app service files
    Added files required to create a module for azure app service

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "azurerm_resource_group" "resource1" {
+  name     = var.resourcename
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storagename
+  resource_group_name      = azurerm_resource_group.resource1.name
+  location                 = azurerm_resource_group.resource1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "storagecontainer" {
+  name                  = var.containername
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_app_service_plan" "appserviceplandemo1" {
+  name                = var.appserviceplanname
+  location            = azurerm_resource_group.resource1.location
+  resource_group_name = azurerm_resource_group.resource1.name
+  kind = "Linux"
+  reserved = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "appservicedemo1" {
+  name                = var.appservicename
+  location            = azurerm_resource_group.resource1.location
+  resource_group_name = azurerm_resource_group.resource1.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplandemo1.id
+
+  site_config {
+    java_version = "11"
+    scm_type     = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,11 @@
+resourcename       = "azureesourcegroup1"
+location           = "Central US"
+storagename        = "azurestoragegrouphiral"
+containername      = "example"
+appserviceplanname = "hiraldemoappserviceplan"
+appservicename     = "hiiraldemoappservice"
+tags = {
+  environment = "Dev"
+  owner       = "Hiral"
+  purpose     = "AzureTfDemo"
+}

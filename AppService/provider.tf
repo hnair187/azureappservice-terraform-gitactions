0e2f136b372b61ee00d3ac918111b2360c7b8479
Hiral Nair <hiralnair@Hirals-MacBook-Air.local>
2021-11-04 14:52:22 +0530

    added values

--- AppService/provider.tf
+++ AppService/provider.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   version = ">=2.40.0"
+  skip_provider_registration = true
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret

612d16543c80d04f785cd800ef757ea9807b0a12
Hiral Nair <hiralnair@Hirals-MacBook-Air.local>
2021-11-04 13:08:56 +0530

    Updated pipeline

--- AppService/backend.tf
+++ AppService/backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/*terraform {
   backend "azurerm" {
     resource_group_name  = "azureesourcegroup1"
     storage_account_name = "azurestoragegrouphiral"
     container_name       = "example"
     key                  = "dev.terraform.tfstate"
   }
-}
+}*/

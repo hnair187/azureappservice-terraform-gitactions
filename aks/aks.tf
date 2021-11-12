a66f95a6d68dc827b55eee5e48983c35009d3bf7
Hiral Nair <hiral.nair@pwc.com>
2021-11-12 15:12:50 +0530

    test

--- aks/aks.tf
+++ aks/aks.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "aksrg" {
 module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = var.resource_group_name
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24"]
+  address_space       = "11.0.0.0/16"
+  subnet_prefixes     = ["11.0.1.0/24"]
   subnet_names        = ["akssubnet1"]
   depends_on          = [azurerm_resource_group.aksrg]
 }

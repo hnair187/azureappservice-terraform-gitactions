7228953d05b3adfe0eafd2c85423e7bacbddd9ee
Hiral Nair <hiral.nair@pwc.com>
2021-11-15 18:33:08 +0530

    testing backend

--- AppService/backend.tf
+++ AppService/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "productzerotfstorage"
     container_name       = "sdm-pwc-container"
     key                  = "appservicedev.terraform.tfstate"
+    access_key = "Q+xSlbNX5FZFmzfWUeL3ECcKbGgz8W+1DcbBqIGfkLLAdMRZTndSazZ7OPUTwXnXclJbmkCpMGAncOP7ZgWsdQ=="
   }
 }

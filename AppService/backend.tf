04a6527504e10b2f6e2e705e32f181bdac5fceae
Hiral Nair <hiral.nair@pwc.com>
2021-11-15 18:04:10 +0530

    testing backend

--- AppService/backend.tf
+++ AppService/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Product_Zero_DevOps"
-    storage_account_name = "productzerostorage"
+    storage_account_name = "productzerotfstorage"
     container_name       = "sdm-pwc-container"
     key                  = "appservicedev.terraform.tfstate"
   }

7432daa3100af8624e160ac4c5dffd4ce78ffdf6
Hiral Nair <hiral.nair@pwc.com>
2021-11-15 17:54:58 +0530

    testing backend

--- Backend/main.tf
+++ Backend/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "storagecontainer" {
   name                  = var.containername
-  storage_account_name  = var.storagename
+  storage_account_name  = azurerm_storage_account.storage.name
   # container_access_type = "blob"
 }
 
